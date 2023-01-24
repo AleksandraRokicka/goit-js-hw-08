0619f81fc08feabf0fe58c1120a44363173afcd4
AleksandraRokicka <rokicka.ola@gmail.com>
2023-01-24 12:13:24 +0100

    poprawa zadania

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,6 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('form');
 const formEmail = document.querySelector('input[name=email]');
 const formMessage = document.querySelector('textarea[name=message]');
-const LOCALSTORAGE_KEY = 'feedback-form-state';
 
 window.addEventListener('load', () => {
   //automatyczne wypelnianie formualrza po wejsciu na stronę(zapamietanie danych). Zapamiętuje NIE WYSŁANE dane, tylko wpisane//
@@ -20,7 +19,7 @@ form.addEventListener(
       email: formEmail.value,
       message: formMessage.value,
     }; //zdarzenie input - po uzupelnieniu formularza, ale NIE WYSLANIU, dane sa zapisane w localstorage i po przeladowaniu strony nadal tam beda - automatycznie sie uzupelnia
-    localStorage.setItem('feedback-form-state', JSON.stringify(someData)); //do local storage wrzucamy feedback-form-state i string wpisanych danych//
+    localStorage.setItem('feedback-form-state', JSON.stringify(someData)); //do local storage wrzucamy feedback-form-state i string wpisanych danych, ustawienie nazwy klucza w local storage.//
   }, 500)
 );
 
@@ -29,5 +28,7 @@ form.addEventListener('submit', e => {
   e.preventDefault();
   console.log(localStorage.getItem('feedback-form-state')); //wrzucam na konsolę obiekt wpisanych daych//
   form.reset(); //zresetowanie danych formularza po wysłaniu-submit//
-  localStorage.clear(); //wyczyczczenie local storage po wysłaniu-submit//
+  localStorage.removeItem('feedback-form-state'); //wyczyszczenie local storage, ale tylko danych formularza, nie usuwa nam to danych z video//
+
+  // localStorage.clear(); //wyczyczczenie CAŁEGO local storage (z video, feedback itp.) po wysłaniu-submit//
 });
